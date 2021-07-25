042bb2c3aed70427da44c15f94d106c37e9b3085
Denis <70014989+warning11223@users.noreply.github.com>
2021-07-25 21:54:18 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+const board = document.querySelector('#board');
+const colors = ['#48d010', '#dcd109', '#0ad970', '#05ffe4', '#0043ad', '#ad0096', '#974c0b'];
+const SQUARES_NUMBER = 800;
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+
+    square.addEventListener('mouseover', () => setColor(square))
+    square.addEventListener('mouseleave', () => removeColor(square))
+
+    board.append(square)
+}
+
+function setColor(element) {
+    const color = getRandomColor()
+    element.style.backgroundColor = color;
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+
+function removeColor(element) {
+    element.style.backgroundColor = '#1d1d1d';
+    element.style.boxShadow = `0 0 2px #000`;
+}
+
+function getRandomColor() {
+    const index = Math.floor(Math.random() * colors.length);
+    return colors[index];
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
